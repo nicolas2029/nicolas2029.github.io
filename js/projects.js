eb023c274b21ee9bc479a6cd07d5fa5858b9de50
Nicolas Gomez Alvarez <nicolas_20_29@hotmail.com>
2022-10-07 11:30:07 -0500

    Added portfolio

---
+++ js/projects.js
@@ -0,0 +1,13 @@
+class Project {
+  constructor(name, descripcion, imgs) {
+    this.name = name;
+    this.descripcion = descripcion;
+    this.imgs = imgs;
+  }
+}
+
+const Projects = new Map({
+  1: { name: "" },
+});
+
+const addCaptures = (project_id) => {};
